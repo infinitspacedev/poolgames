7f85745aec69797e7061016fcb37e460c9385939
kmaleao <udijonson@gmail.com>
2022-05-15 20:19:39 -0300

    primeiro commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home(){
+    return(
+        <div>
+            <h1>Simpsons 1 temporada</h1>
+            <img src='/simpson.jpg'></img>
+            <a href='/simpson1.mp4'download>Episodio primeirissimo(01) BAIXE AGORA!!!</a>
+        </div>
+    )
+}
